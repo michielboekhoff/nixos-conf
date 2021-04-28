5fb03b2acb8bbc8c71794bd1c27e16d42b561dbc
Michiel Boekhoff <michielalexanderb@gmail.com>
2021-04-28 16:14:14 +0100

    Set default editor to vim.

--- modules/editors/default.nix
+++ modules/editors/default.nix
@@ -5,7 +5,7 @@ with lib.my;
 let cfg = config.modules.editors;
 in {
   options.modules.editors = {
-    default = mkOpt types.str "emacs";
+    default = mkOpt types.str "vim";
   };
 
   config = mkIf (cfg.default != null) {
