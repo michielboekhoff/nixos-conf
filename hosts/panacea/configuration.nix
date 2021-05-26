97fee908e645072e04f824fab2cc5dc025f34bb6
Michiel Boekhoff <michielalexanderb@gmail.com>
2021-05-26 20:31:49 +0100

    Rename gnome3 to gnome as per nixpkgs rename.

--- hosts/panacea/configuration.nix
+++ hosts/panacea/configuration.nix
@@ -39,7 +39,7 @@
 
   # Enable the GNOME 3 Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome3.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   # services.xserver.layout = "us";
