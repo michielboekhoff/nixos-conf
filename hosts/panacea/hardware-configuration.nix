8b1dd997bcd2b3abf05e9310f05b82ffe2093803
Michiel Boekhoff <michielalexanderb@gmail.com>
2021-10-19 14:01:35 +0100

    Add groups & users for printing, scanning and Podman.

--- hosts/panacea/hardware-configuration.nix
+++ hosts/panacea/hardware-configuration.nix
@@ -5,6 +5,13 @@
 
   hardware.system76.enableAll = true;
 
+  # Scanner support
+  hardware.sane.enable = true;
+  hardware.sane.extraBackends = [ pkgs.epkowa ];
+
+  # Needed to access the scanner without root
+  users.users.michiel.extraGroups = [ "scanner" "lp" "podman" ];
+
   boot.initrd.availableKernelModules =
     [ "xhci_pci" "ahci" "nvme" "usbhid" "uas" "sd_mod" "sdhci_pci" ];
   boot.initrd.kernelModules =
