9c24e1d7f63368da3217e764b1b1b7ce5062deb9
Michiel Boekhoff <michielalexanderb@gmail.com>
2022-11-06 16:30:50 +0000

    Add C# plugin to vscode

--- hosts/kyoshi/default.nix
+++ hosts/kyoshi/default.nix
@@ -1,9 +1,10 @@
 { pkgs, config, lib, ... }:
 
-let cfg = config.dotfiles;
-    gcloud = pkgs.local.google-cloud-sdk.override {
-      #with-numpy = true;
-    };
+let
+  cfg = config.dotfiles;
+  gcloud = pkgs.local.google-cloud-sdk.override {
+    #with-numpy = true;
+  };
 in {
   imports = [ ./configuration.nix ./hardware-configuration.nix ];
 
@@ -13,95 +14,100 @@ in {
     config = cfg;
   };
 
+  environment = {
+    systemPackages = with pkgs; [
+      #(gcloud.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
+      kubectl
+      kubectx
+      teams
+      zoom-us
+      google-chrome
+      discord
+      ssm-session-manager-plugin
+      xclip
+      vim
+    ];
 
- environment = {
-   systemPackages = with pkgs; [
-     #(gcloud.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
-     kubectl
-     kubectx
-     teams
-     zoom-us
-     google-chrome
-     discord
-     ssm-session-manager-plugin
-     xclip
-     vim
-   ];
+    shellAliases = {
+      github = "cd ~/git/github.com";
+      gitroot = "cd ~/git";
+    };
+  };
 
-   shellAliases = {
-     github = "cd ~/git/github.com";
-     gitroot = "cd ~/git";
-   };
- };
+  # To use the Yubikey as a smart card
+  services.pcscd.enable = true;
 
- # To use the Yubikey as a smart card
- services.pcscd.enable = true;
+  modules = {
+    editors = {
+      emacs = {
+        enable = true;
+        daemon = true;
+      };
+      intellij.enable = true;
+      vim.enable = true;
+      android-studio.enable = true;
+      vscode = {
+        enable = true;
+        extensions = [
+          {
+            name = "terraform";
+            publisher = "Hashicorp";
+            version = "2.23.0";
+            sha256 = "sha256-3v2hEf/cEd7NiXfk7eJbmmdyiQJ7bWl9TuaN+y5k+e0=";
+          }
+          {
+            name = "vscode-docker";
+            publisher = "ms-azuretools";
+            version = "1.22.1";
+            sha256 = "sha256-okR1mmwg1ZEUfP924LTa98LxCenwDZ1BIS/FLt0wo8c=";
+          }
+          {
+            name = "python";
+            publisher = "ms-python";
+            version = "2022.11.12011103";
+            sha256 = "sha256-VGAMbg2Gwdi0PST+EdKMY5Cuw5GUMUJcZoN4SSSMOIk=";
+          }
+          {
+            name = "direnv";
+            publisher = "mkhl";
+            version = "0.6.1";
+            sha256 = "5/Tqpn/7byl+z2ATflgKV1+rhdqj+XMEZNbGwDmGwLQ=";
+          }
+          {
+            name = "csharp";
+            publisher = "ms-dotnettools";
+            version = "1.25.0";
+            sha256 = "sha256-WE4DbJr1HqoyuS3mVYmIgd9DDTuSKPd6vdZn4YOPUtU=";
+          }
+        ];
+      };
+    };
 
- modules = {
-   editors = {
-     emacs = {
-       enable = true;
-       daemon = true;
-     };
-     intellij.enable = true;
-     vim.enable = true;
-     android-studio.enable = true;
-     vscode = {
-       enable = true;
-       extensions = [
-         {
-           name = "terraform";
-           publisher = "Hashicorp";
-           version = "2.23.0";
-           sha256 = "sha256-3v2hEf/cEd7NiXfk7eJbmmdyiQJ7bWl9TuaN+y5k+e0=";
-         }
-         {
-           name = "vscode-docker";
-           publisher = "ms-azuretools";
-           version = "1.22.1";
-           sha256 = "sha256-okR1mmwg1ZEUfP924LTa98LxCenwDZ1BIS/FLt0wo8c=";
-         }
-         {
-           name = "python";
-           publisher = "ms-python";
-           version = "2022.11.12011103";
-           sha256 = "sha256-VGAMbg2Gwdi0PST+EdKMY5Cuw5GUMUJcZoN4SSSMOIk=";
-         }
-         {
-           name = "direnv";
-           publisher = "mkhl";
-           version = "0.6.1";
-           sha256 = "5/Tqpn/7byl+z2ATflgKV1+rhdqj+XMEZNbGwDmGwLQ=";
-         }
-       ];
-     };
-   };
+    shell = {
+      zsh.enable = true;
+      kitty.enable = true;
+    };
 
-   shell = {
-     zsh.enable = true;
-     kitty.enable = true;
-   };
+    torrent = { qbittorrent.enable = true; };
 
-   torrent = { qbittorrent.enable = true; };
+    dev = {
+      terraform.enable = true;
+      aws.enable = true;
+      docker = {
+        enable = true;
+        docker-compose = true;
+      };
+    };
 
-   dev = {
-     terraform.enable = true;
-     aws.enable = true;
-     docker = {
-       enable = true;
-       docker-compose = true;
-     };
-   };
+    media = { spotify.enable = true; };
 
-   media = { spotify.enable = true; };
-
-   hardware = {
-     moonlander.enable = true;
-     scanning.enable = true;
-     printing = {
-       enable = true;
-       drivers = with pkgs; [ brgenml1cupswrapper brgenml1lpr ];
-     };
-   };
- };
+    hardware = {
+      moonlander.enable = true;
+      scanning.enable = true;
+      printing = {
+        enable = true;
+        drivers = with pkgs; [ brgenml1cupswrapper brgenml1lpr ];
+      };
+    };
+  };
 }
