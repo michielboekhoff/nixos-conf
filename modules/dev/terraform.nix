d5655614ff0a9bfc803457f7f1e1cb579c97ec43
Michiel Boekhoff <michielalexanderb@gmail.com>
2023-05-23 06:42:32 +0100

    Upgrade terraform-ls version

--- modules/dev/terraform.nix
+++ modules/dev/terraform.nix
@@ -1,12 +1,29 @@
-{ config, options, lib, pkgs, my, ... }:
+{ config, lib, pkgs, ... }:
 
 with lib;
 with lib.my;
 let cfg = config.modules.dev.terraform;
+		terraformls = let
+			version = "0.31.2";
+			src = pkgs.fetchFromGitHub {
+				owner = "hashicorp";
+				repo = "terraform-ls";
+				rev = "v${version}";
+				sha256 = "sha256-VqyRD2G+5L79Wm4JrZ2/PC8VeT+j/p0d22ZO5hy1Pf0=";
+			};
+		in
+			pkgs.terraform-ls.override {
+				buildGoModule = args: pkgs.buildGoModule ( args // {
+					inherit src version;
+					vendorSha256 = "sha256-s881Wtds33Q/Rh6sH3lHWotX3eVVs9ogzJaOjtJ8s4U=";
+					ldflags = [ "-s" "-w" ];
+					doInstallCheck = false;
+				});
+			};
 in {
   options.modules.dev.terraform = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
-    user.packages = with pkgs; [ terraform terraform-ls ];
+    user.packages = with pkgs; [ terraform terraformls ];
   };
 }
